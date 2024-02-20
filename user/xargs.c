dac4a85986725ff01376eb55fb1d5ae7d42b44c3
Simi <minhvip08@gmail.com>
2024-02-20 13:14:10 +0700

    xargs

--- user/xargs.c
+++ user/xargs.c
@@ -1,66 +1,47 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/param.h"  // MAXARG
+#include "kernel/param.h" // MAXARG
 
-#define is_blank(chr) (chr == ' ' || chr == '\t') 
-
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  // Declare and initialize variables
-  char buf[2048] = {0}; // buffer to store input
-  char ch; // character to read from input
-  char *p = buf; // pointer to current position in buffer
-  char *v[MAXARG]; // array to store arguments for command
-  int c = 0; // counter for arguments
-  int blanks = 0; // flag for blank characters
-  int offset = 0; // offset for buffer
+  char reader;
+  char buff[2048] = {0};
+  char *p = buff;
+  char *v[MAXARG];
+  int c = 0;
+  int blanks = 0;
+  int offset = 0;
 
-  // Check if there is at least one argument for command
-  if(argc <= 1){
+  if (argc <= 1)
+  {
     exit(1);
   }
 
-  // Copy arguments from argv to v, skipping the first one
-  for (c = 1; c < argc; c++) {
-    v[c-1] = argv[c];
+  for (c = 1; c < argc; c++)
+  {
+    v[c - 1] = argv[c];
   }
-  c--; // decrement c to point to the last argument in v
-
-  // Read input one character at a time
-  while (read(0, &ch, 1) > 0) {
-    // If character is blank, set blanks to 1 and skip it
-    if (is_blank(ch)) {
+  c--;
+  while (read(0, &reader, 1) > 0)
+  {
+    if (reader == ' ' || reader == '\t')
       blanks = 1;
-      continue;
-    }
-
-    // If there were blanks before, add a null terminator to buffer
-    // and set the next argument in v to point to the current position in buffer
-    // Reset blanks and p
-    if (blanks) { 
-      buf[offset++] = '\0';
+    if (blanks)
+    {
+      buff[offset++] = '\0';
       v[c++] = p;
-      p = buf + offset;
+      p = buff + offset;
       blanks = 0;
     }
-
-    // If character is not a newline, add it to buffer
-    if (ch != '\n') {
-      buf[offset++] = ch;
-    } 
-    // If character is a newline, end of input line reached
-    else {
-      // Set the next argument in v to point to the current position in buffer
-      // Reset p
-      v[c++] = p;
-      p = buf + offset;
-
-      // Fork a child process and execute the command in v
-      // Wait for the child process to finish
-      // Reset c to point to the last argument in v
-      if (fork() == 0) {
+    if (reader != '\n')
+      buff[offset++] = reader;
+    else
+    {
+      v[c] = p;
+      p = buff + offset;
+      if (fork() == 0)
+      {
         exec(v[0], v);
         exit(0);
       }
@@ -68,7 +49,5 @@ main(int argc, char *argv[])
       c = argc - 1;
     }
   }
-
-  // Exit the program
   exit(0);
 }
